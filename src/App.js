c53688195031240d9e8190ab0f09896cbbe56bfd
Posso <micposso@gmail:.com>
2018-07-26 16:45:46 -0400

    included search compnent in the main app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
-import SearchComponent from './components/SearchComponent.js'
+//import SearchComponent from './components/SearchComponent.js'
 import BookShelfComponent from './components/BookShelfComponent'
 import './css/App.css'
 //import { getAll } from './BooksAPI.js';
@@ -10,8 +10,38 @@ import './css/App.css'
 class BooksApp extends React.Component {
   state = {
     books: [],
-    message: "You dont have any books on this shelf"
+    message: "You dont have any books on this shelf",
+    searchQuery: '',
+    displayBooks: [],
   }
+
+  updateSearch = (query) => {
+
+    if ( query.length >= 0 ) {
+        this.setState({
+            searchQuery: query
+        });
+        BooksAPI.search(query, 20).then((searchedBooks) => {
+            const displayBooks = searchedBooks ? searchedBooks : [];
+            if ( searchedBooks.length > 0 ) {
+                const books = this.state.books;
+                    for ( const displayBook of displayBooks) {
+                        const shelfBook = books.find(book => book.id === displayBook.id )
+                        if ( shelfBook ) {
+                            displayBook.shelf = shelfBook.shelf;
+                        }
+                        this.setState({ displayBooks });
+                    }
+            }
+            
+        })
+    } else {
+        this.setState({
+            displayBooks: []
+        });
+    }
+
+}
 
   handleChangeShelve(book, shelf) {
     BooksAPI.update(book, shelf).then(() => {
@@ -30,10 +60,50 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route exact path="/search" render={() => (
-          <SearchComponent message={this.state.message} bookList={this.state.books} handleChangeShelve={this.handleChangeShelve.bind(this)}/>
-        )}/>
-        <Route exact path="/" render={() => (
+
+              {/* start of the seach component */}
+              <div>
+                <div className="search-books">
+                    <div className="search-books-bar">
+                        {/*<Link to="/" className="close-search">Close</Link>*/}
+                        <div className="search-books-input-wrapper">
+                            <input 
+                                type="text"
+                                value={this.state.searchQuery}
+                                onChange={(event) => this.updateSearch(event.target.value)}
+                            />
+                        </div>
+                    </div>
+                <div className="search-books-results">
+                <ol className="books-grid">
+                <h2>{this.state.notFound}</h2>
+                { this.state.displayBooks ? 
+                this.state.displayBooks.map((book, index) => 
+                <li key={index}>
+                    <div className="book">
+                        <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                            <select onChange={(e) => this.props.handleChangeShelve(book, e.target.value)}>
+                                <option>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                </li>
+                ) : <h2>Type a search term to display books</h2> }
+            </ol>
+                </div>
+                </div>
+          </div>{/* end of the seach component */}
+
+
           <div>
             <div className="list-books">
               <div className="list-books-title">
@@ -49,7 +119,7 @@ class BooksApp extends React.Component {
               <Link to="/search">Add a book</Link>
             </div>
         </div>
-        )}/>
+        
       </div>
     )
   }
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -8,83 +8,18 @@ import sortBy from 'sort-by'
 class SearchComponent extends Component{
 
     state = {
-        searchQuery: '',
-        displayBooks: [],
-    }
 
-    updateSearch = (query) => {
 
-        if ( query.length >= 0 ) {
-            this.setState({
-                searchQuery: query
-            });
-            BooksAPI.search(query, 20).then((searchedBooks) => {
-                const displayBooks = searchedBooks ? searchedBooks : [];
-                if ( searchedBooks.length > 0 ) {
-                    const books = this.props.bookList;
-                        for ( const displayBook of displayBooks) {
-                            const shelfBook = books.find(book => book.id === displayBook.id )
-                            if ( shelfBook ) {
-                                displayBook.shelf = shelfBook.shelf;
-                            }
-                            this.setState({ displayBooks });
-                        }
-                }
-                
-            })
-        } else {
-            this.setState({
-                displayBooks: []
-            });
-        }
 
     }
+
+
 
     render() {
         return(
-            <div>
-                <div className="search-books">
-                    <div className="search-books-bar">
-                        <Link to="/" className="close-search">Close</Link>
-                        <div className="search-books-input-wrapper">
-                            <input 
-                                type="text"
-                                value={this.state.searchQuery}
-                                onChange={(event) => this.updateSearch(event.target.value)}
-                            />
-                        </div>
-                    </div>
-                <div className="search-books-results">
-                <ol className="books-grid">
-                <h2>{this.state.notFound}</h2>
-                { this.state.displayBooks ? 
-                this.state.displayBooks.map((book, index) => 
-                <li key={index}>
-                    <div className="book">
-                        <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                            <select onChange={(e) => this.props.handleChangeShelve(book, e.target.value)}>
-                                <option>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
-                </li>
-                ) : <h2>Type a search term to display books</h2> }
 
+            //paste JSX here
 
-         
-            </ol>
-                </div>
-                </div>
-          </div>
         )
     }
 }
